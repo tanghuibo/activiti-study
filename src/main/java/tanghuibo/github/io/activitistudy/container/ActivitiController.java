73810c73dd328eaac80f5fd7ae7241823412bd3c
tang_huibo <1215893264@qq.com>
2020-04-17 23:47:10 +0800

    抽取convert工具类

--- src/main/java/tanghuibo/github/io/activitistudy/container/ActivitiController.java
+++ src/main/java/tanghuibo/github/io/activitistudy/container/ActivitiController.java
@@ -6,6 +6,7 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.springframework.web.bind.annotation.*;
 import tanghuibo.github.io.activitistudy.entity.*;
 import tanghuibo.github.io.activitistudy.service.ActivitiService;
+import tanghuibo.github.io.activitistudy.utils.ConvertTool;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -24,12 +25,12 @@ public class ActivitiController {
 
     @GetMapping("queryDeployment")
     public List<DeploymentDTO> queryDeployment(DeploymentQueryParam param) {
-        return activitiService.queryDeployment(param).stream().map(this::convertToDTO).collect(Collectors.toList());
+        return activitiService.queryDeployment(param).stream().map(ConvertTool::convertToDTO).collect(Collectors.toList());
     }
 
     @GetMapping("queryProcessDefinition")
     public List<ProcessDefinitionDTO> queryProcessDefinition(ProcessDefinitionQueryParam param) {
-        return activitiService.queryProcessDefinition(param).stream().map(this::convertToDTO).collect(Collectors.toList());
+        return activitiService.queryProcessDefinition(param).stream().map(ConvertTool::convertToDTO).collect(Collectors.toList());
     }
 
     @PostMapping("deleteDeploymentById")
@@ -39,67 +40,11 @@ public class ActivitiController {
 
     @PostMapping("addDeployment")
     public DeploymentDTO addDeployment(@RequestBody DeploymentAddParam param) {
-        return convertToDTO(activitiService.addDeployment(param));
+        return ConvertTool.convertToDTO(activitiService.addDeployment(param));
     }
 
     @PostMapping("startProcessInstance")
     public ProcessInstanceDTO startProcessInstance(@RequestBody DeploymentTaskParam param) {
-        return convertToDTO(activitiService.startProcessInstance(param));
-    }
-
-    private ProcessDefinitionDTO convertToDTO(ProcessDefinition processDefinition) {
-        ProcessDefinitionDTO processDefinitionDTO = new ProcessDefinitionDTO();
-        processDefinitionDTO.setId(processDefinition.getId());
-        processDefinitionDTO.setCategory(processDefinition.getCategory());
-        processDefinitionDTO.setName(processDefinition.getName());
-        processDefinitionDTO.setKey(processDefinition.getKey());
-        processDefinitionDTO.setDescription(processDefinition.getDescription());
-        processDefinitionDTO.setVersion(processDefinition.getVersion());
-        processDefinitionDTO.setResourceName(processDefinition.getResourceName());
-        processDefinitionDTO.setDeploymentId(processDefinition.getDeploymentId());
-        processDefinitionDTO.setDiagramResourceName(processDefinition.getDiagramResourceName());
-        processDefinitionDTO.setHasStartFormKey(processDefinition.hasStartFormKey());
-        processDefinitionDTO.setHasGraphicalNotation(processDefinition.hasGraphicalNotation());
-        processDefinitionDTO.setSuspended(processDefinition.isSuspended());
-        processDefinitionDTO.setTenantId(processDefinition.getTenantId());
-        processDefinitionDTO.setEngineVersion(processDefinition.getEngineVersion());
-        processDefinitionDTO.setAppVersion(processDefinition.getAppVersion());
-        return processDefinitionDTO;
-    }
-
-    private DeploymentDTO convertToDTO(Deployment deployment) {
-        DeploymentDTO dto = new DeploymentDTO();
-        dto.setId(deployment.getId());
-        dto.setName(deployment.getName());
-        dto.setDeploymentTime(deployment.getDeploymentTime());
-        dto.setCategory(deployment.getCategory());
-        dto.setKey(deployment.getKey());
-        dto.setTenantId(deployment.getTenantId());
-        dto.setVersion(deployment.getVersion());
-        dto.setProjectReleaseVersion(deployment.getProjectReleaseVersion());
-        return dto;
-    }
-
-    private ProcessInstanceDTO convertToDTO(ProcessInstance processInstance) {
-        ProcessInstanceDTO dto = new ProcessInstanceDTO();
-        dto.setProcessDefinitionId(processInstance.getProcessDefinitionId());
-        dto.setProcessDefinitionName(processInstance.getProcessDefinitionName());
-        dto.setProcessDefinitionKey(processInstance.getProcessDefinitionKey());
-        dto.setProcessDefinitionVersion(processInstance.getProcessDefinitionVersion());
-        dto.setDeploymentId(processInstance.getDeploymentId());
-        dto.setBusinessKey(processInstance.getBusinessKey());
-        dto.setProcessVariables(processInstance.getProcessVariables());
-        dto.setTenantId(processInstance.getTenantId());
-        dto.setName(processInstance.getName());
-        dto.setDescription(processInstance.getDescription());
-        dto.setLocalizedName(processInstance.getLocalizedName());
-        dto.setLocalizedDescription(processInstance.getLocalizedDescription());
-        dto.setStartTime(processInstance.getStartTime());
-        dto.setSuspended(processInstance.isSuspended());
-        dto.setStartUserId(processInstance.getStartUserId());
-        dto.setAppVersion(processInstance.getAppVersion());
-        return dto;
-
-
+        return ConvertTool.convertToDTO(activitiService.startProcessInstance(param));
     }
 }
